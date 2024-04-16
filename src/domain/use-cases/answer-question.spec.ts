b8a8596a41581dbf652105f4428e5eb33a4e9e9b
Samuel Lima <samudomingos2018@gmail.com>
2024-04-16 14:14:34 -0300

    feat: add connection to external dependencies (repositories)

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,11 +1,18 @@
 import { expect, test } from 'vitest'
 import { AnswerQuestionUseCase } from './answer-question'
+import { AnswersRepository } from '../repositories/answers-repository'
 
-test('create an answer',()=>{
-    const answerQuestion = new AnswerQuestionUseCase()
+const fakeAnswersRepository: AnswersRepository = {
+    create: async (answer) => {
+        return
+    }
+}
 
-    const answer = answerQuestion.execute({
-        instrcutorId: '1',
+test('create an answer',async ()=>{
+    const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
+
+    const answer = await answerQuestion.execute({
+        instructorId: '1',
         questionId: '2',
         content: 'New answer'
     })
--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,15 +1,26 @@
 import { Answer } from "../entitites/answer";
+import { AnswersRepository } from "../repositories/answers-repository";
 
 interface AnswerQuestionUseCaseRequest {
-    instrcutorId: string;
+    instructorId: string;
     questionId: string;
     content: string;
 }
 
 export class AnswerQuestionUseCase {
 
-    execute({ instrcutorId, questionId, content }: AnswerQuestionUseCaseRequest) {
-        const anwser = new Answer(content)
+    constructor(private answersRepository: AnswersRepository){
+
+    }
+
+    async execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
+        const anwser = new Answer({
+            authorId: instructorId,
+            questionId,
+            content
+        })
+
+        await this.answersRepository.create(anwser)
 
         return anwser
     }
