086ec85829f5c6dc6e8f9a45bb6f9f54f259521c
Samuel Lima <samudomingos2018@gmail.com>
2024-04-18 16:22:53 -0300

    chore: apply OOP concepts to the 'questions' and 'answers' entities

--- src/domain/forum/enterprise/entities/question-comment.ts
+++ src/domain/forum/enterprise/entities/question-comment.ts
@@ -1,43 +1,14 @@
-import { Entity } from '@/core/entities/entity'
 import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
+import { Comment, CommentProps } from './comment'
 
-export interface QuestionCommentProps {
-  authorId: UniqueEntityId
-  anwserId: UniqueEntityId
-  content: string
-  createdAt: Date
-  updatedAt?: Date
+export interface QuestionCommentProps extends CommentProps {
+  questionId: UniqueEntityId
 }
 
-export class QuestionComment extends Entity<QuestionCommentProps> {
-  get authorId() {
-    return this.props.authorId
-  }
-
-  get content() {
-    return this.props.content
-  }
-
-  get createdAt() {
-    return this.props.createdAt
-  }
-
-  get updatedAt() {
-    return this.props.updatedAt
-  }
-
-  get excerpt() {
-    return this.content.substring(0, 120).trim().concat('...')
-  }
-
-  private touch() {
-    this.props.updatedAt = new Date()
-  }
-
-  set content(value: string) {
-    this.props.content = value
-    this.touch()
+export class QuestionComment extends Comment<QuestionCommentProps> {
+  get questionId() {
+    return this.props.questionId
   }
 
   static create(
