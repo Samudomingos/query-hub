1c2c00bed34d3a05940cf0d9d27c97a7d2e7d776
Samuel Lima <samudomingos2018@gmail.com>
2024-04-18 12:04:31 -0300

    feat: add delete question use case

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -4,11 +4,22 @@ import { Question } from '@/domain/forum/enterprise/entities/question'
 export class InMemoryQuestionsRepository implements QuestionsRepository {
   public items: Question[] = []
 
-  async create(question: Question) {
-    this.items.push(question)
+  async findById(id: string): Promise<Question | null> {
+    return this.items.find((question) => question.id.toValue() === id) || null
   }
 
   async findBySlug(slug: string): Promise<Question | null> {
     return this.items.find((question) => question.slug.value === slug) || null
+  }
+
+  async create(question: Question) {
+    this.items.push(question)
+  }
+
+  async delete(question: Question): Promise<void> {
+    const questionIndex = this.items.findIndex(
+      (item) => item.id === question.id,
+    )
+    this.items.splice(questionIndex, 1)
   }
 }
