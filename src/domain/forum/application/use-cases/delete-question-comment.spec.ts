88024e0ae838ed025f150b90c3c4e15f99719f4a
Samuel Lima <samudomingos2018@gmail.com>
2024-04-18 17:10:51 -0300

    feat: add the delete question comment use case

---
+++ src/domain/forum/application/use-cases/delete-question-comment.spec.ts
@@ -0,0 +1,48 @@
+import { makeQuestion } from 'test/factories/make-question'
+
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { DeleteQuestionCommentUseCase } from './delete-question-comment'
+import { InMemoryQuestionCommentsRepository } from 'test/repositories/in-memory-question-comments-repository'
+import { makeQuestionComment } from 'test/factories/make-question-comment'
+
+let inMemoryQuestionsCommentsRepository: InMemoryQuestionCommentsRepository
+let sut: DeleteQuestionCommentUseCase
+
+describe('Delete Question Comment Use Case', async () => {
+  beforeEach(() => {
+    inMemoryQuestionsCommentsRepository =
+      new InMemoryQuestionCommentsRepository()
+    sut = new DeleteQuestionCommentUseCase(inMemoryQuestionsCommentsRepository)
+  })
+
+  it('should be able to delete a question comment', async () => {
+    const newQuestionComment = makeQuestionComment()
+
+    await inMemoryQuestionsCommentsRepository.create(newQuestionComment)
+
+    await sut.execute({
+      questionCommentId: newQuestionComment.id.toString(),
+      authorId: newQuestionComment.authorId.toString(),
+    })
+
+    expect(inMemoryQuestionsCommentsRepository.items).toHaveLength(0)
+  })
+
+  it('should not be able to delete another user question comment', async () => {
+    const newQuestion = makeQuestionComment(
+      {
+        authorId: new UniqueEntityId('author-1'),
+      },
+      new UniqueEntityId('question-1'),
+    )
+
+    await inMemoryQuestionsCommentsRepository.create(newQuestion)
+
+    await expect(() =>
+      sut.execute({
+        questionCommentId: 'question-1',
+        authorId: 'author-2',
+      }),
+    ).rejects.toBeInstanceOf(Error)
+  })
+})
--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -5,6 +5,20 @@ export class InMemoryQuestionCommentsRepository
   implements QuestionCommentsRepository
 {
   public items: QuestionComment[] = []
+  async findById(id: string) {
+    return (
+      this.items.find(
+        (questionComment) => questionComment.id.toString() === id,
+      ) || null
+    )
+  }
+
+  async delete(questionComment: QuestionComment) {
+    const questionCommentIndex = this.items.findIndex(
+      (item) => item.id === questionComment.id,
+    )
+    this.items.splice(questionCommentIndex, 1)
+  }
 
   async create(questionComment: QuestionComment) {
     this.items.push(questionComment)
