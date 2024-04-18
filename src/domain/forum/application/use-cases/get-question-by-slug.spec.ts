089e86ff63921b54e84b914a3668540c6995a4d6
Samuel Lima <samudomingos2018@gmail.com>
2024-04-18 11:08:06 -0300

    feat: add get question by slug use case

---
+++ src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -0,0 +1,32 @@
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { GetQuestionBySlugUseCase } from './get-question-by-slug'
+import { Question } from '../../enterprise/entities/question'
+import { Slug } from '../../enterprise/entities/value-objects/slug'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: GetQuestionBySlugUseCase
+
+describe('Get Question By Slug Use Case', async () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new GetQuestionBySlugUseCase(inMemoryQuestionsRepository)
+  })
+
+  it('should be able get a question by slug', async () => {
+    const newQuestion = Question.create({
+      authorId: new UniqueEntityId('1'),
+      title: 'New question',
+      slug: Slug.create('example-question'),
+      content: 'New content',
+    })
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    const { question } = await sut.execute({
+      slug: 'example-question',
+    })
+
+    expect(question.id).toBeTruthy()
+    expect(question.title).toEqual(question.title)
+  })
+})
